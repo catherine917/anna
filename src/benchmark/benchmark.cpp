ff0b9a608a39306c4a7583b969e3d8a51ddd496d
zw <catherine_8917@163.com>
2020-10-14 17:42:58 +0800

    update

--- src/benchmark/benchmark.cpp
+++ src/benchmark/benchmark.cpp
@@ -200,12 +200,12 @@ void run(const unsigned &thread_id,
         // }
         
         log->info("Start benchmarking");
-        auto benchmark_start = std::chrono::system_clock::now();
         // vector<string> keys;
         unsigned loop = 20;
         unsigned num_reqs = num_keys / loop;
         log->info("Number of requests per loop is {}", num_reqs);
         unsigned loop_counter = 0; 
+        auto benchmark_start = std::chrono::system_clock::now();
         while (loop_counter < loop) {
             for(unsigned i = 0; i < num_reqs; i++) {
             // log->info("index is {}", i);
@@ -220,6 +220,7 @@ void run(const unsigned &thread_id,
                 unsigned ts = generate_timestamp(thread_id);
                 LWWPairLattice<string> val(
                     TimestampValuePair<string>(ts, string(length, 'a')));
+                benchmark_start = std::chrono::system_clock::now();
                 client.put_async(key, serialize(val), LatticeType::LWW);
                 receive_key_addr(&client, key);
                 client.get_async(key);
